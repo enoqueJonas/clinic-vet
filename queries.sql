797efee0a9f3d1064e6724c6d654c739bea4969d
enoqueJonas <enoque.macanda@gmail.com>
2023-01-31 18:30:26 +0200

    ADD querries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals 
+    WHERE name like '%mon';
+
+SELECT name from animals 
+    WHERE EXTRACT('Year' from date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT name from animals 
+    WHERE neutered = true AND escape_attemps < 3;
+
+SELECT date_of_birth from animals 
+    WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attemps from animals 
+    WHERE meightk_kg > 10.5;
+
+SELECT * from animals 
+    WHERE neutered = true;
+
+SELECT * from animals 
+    WHERE name != 'Gabumon';
+
+SELECT * from animals 
+    WHERE meightk_kg >= 10.4 AND meightk_kg <= 17.3;
