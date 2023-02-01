59100d87483ce17c26a71380f444d126dd5435c5
enoqueJonas <enoque.macanda@gmail.com>
2023-02-01 16:25:36 +0200

    UPDATE animals queries

--- queries.sql
+++ queries.sql
@@ -23,3 +23,43 @@ SELECT * from animals
 
 SELECT * from animals 
     WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* ------------------------------------------------------------- */
+BEGIN;
+
+UPDATE animals set species = 'unspecified';
+
+select * from animals;
+
+ROLLBACK;
+
+/* ------------------------------------------------------------- */
+
+BEGIN;
+
+UPDATE animals set species = 'digimon'
+    WHERE name like '%mon';
+
+UPDATE animals set species = 'pokemon'
+    WHERE species is NULL;
+
+COMMIT;
+
+/* ------------------------------------------------------------- */
+
+SELECT count(*) from animals;
+
+SELECT count(escape_attemps) from animals
+    WHERE escape_attemps = 0;
+
+SELECT AVG(meightk_kg) from animals;
+
+SELECT neutered, count(escape_attemps) from animals
+    GROUP BY neutered;
+
+SELECT species, MAX(meightk_kg), MIN(meightk_kg) from animals
+    GROUP BY species;
+
+SELECT species, AVG(escape_attemps) from animals
+    WHERE EXTRACT('Year' from date_of_birth) BETWEEN 1990 AND 2000
+    GROUP BY species;
